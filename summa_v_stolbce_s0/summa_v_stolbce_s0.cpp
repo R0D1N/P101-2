9e7e1f0425cd5e67ef557d9bff051df22f920616
Kiril <irodin007@gmail.com>
2020-02-22 20:00:13 +0300

    alpha release

--- summa_v_stolbce_s0/summa_v_stolbce_s0.cpp
+++ summa_v_stolbce_s0/summa_v_stolbce_s0.cpp
@@ -1,7 +1,47 @@
 ﻿#include <iostream>
 
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int rows = 4;
+    int columns = 4;
+    double **matrix = new double*[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = new double[columns];
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int sum = 0;
+    int rows_counter = 0;
+
+    for (size_t j = 0; j < columns; j++)
+    {
+        if (matrix[rows_counter][j] == 0)
+        {
+            sum += j+1;
+        }
+        rows_counter++;
+    }
+
+    cout << endl << sum << endl;
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    system("pause");
+    return 0;
 }
