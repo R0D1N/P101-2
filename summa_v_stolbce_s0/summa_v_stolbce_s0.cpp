1bb800f77b258db50a2cfe7e30947307ad3d3cc7
Kiril <irodin007@gmail.com>
2020-02-25 21:46:59 +0300

    Functions have been added Work about second method in progress

--- summa_v_stolbce_s0/summa_v_stolbce_s0.cpp
+++ summa_v_stolbce_s0/summa_v_stolbce_s0.cpp
@@ -2,46 +2,130 @@
 
 using namespace std;
 
-int main()
-{
-    int rows = 4;
-    int columns = 4;
-    double **matrix = new double*[rows];
-    for (int i = 0; i < rows; i++)
-    {
-        matrix[i] = new double[columns];
-    }
+int sumFunc_1(int rows, int columns, double** matrix) {
+	int counter = 0;
+	if (rows > columns) {
+		counter = columns;
+	}
+	if (rows < columns) {
+		counter = rows;
+	}
+	else
+	counter = columns;
 
-    for (int i = 0; i < rows; i++)
-    {
-        for (size_t j = 0; j < columns; j++)
-        {
-            cin >> matrix[i][j];
-        }
-    }
+	int sum = 0;
 
-    int sum = 0;
-    int rows_counter = 0;
+	for (int j = 0; j < counter; j++) {
+		if (matrix[j][j] == 0)
+		{
+			sum += j + 1;
+		}
+	}
 
-    for (size_t j = 0; j < columns; j++)
-    {
-        if (matrix[rows_counter][j] == 0)
-        {
-            sum += j+1;
-        }
-        rows_counter++;
-    }
+	if (rows < columns) {
+		matrix[rows - 1][rows - 1] = sum;
+	}
 
-    cout << endl << sum << endl;
+	if (rows > columns) {
+		matrix[columns - 1][columns - 1] = sum;
+	}
+	return sum;
+}
 
-    for (size_t i = 0; i < rows; i++)
-    {
-        for (size_t j = 0; j < columns; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-    system("pause");
-    return 0;
+//int sumFunc_2(int rows, int columns, double* matrix) {
+//	int counter = 0;
+//	if (rows > columns) {
+//		counter = columns;
+//	}
+//	if (rows < columns) {
+//		counter = rows;
+//	}
+//	else
+//		counter = columns;
+//
+//	int sum = 0;
+//
+//	for (int j = 0; j < counter; j++) {
+//		if (&matrix[j * columns + j] == 0)
+//		{
+//			sum += j + 1;
+//		}
+//	}
+//
+//	if (rows < columns) {
+//		&matrix[rows * columns + ] = sum;
+//	}
+//
+//	if (rows > columns) {
+//		&matrix[rows * columns] = sum;
+//	}
+//	return sum;
+//}
+
+void fillArray_1(int rows, int columns, double** matrix) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+}
+
+void fillArray_2(int rows, int columns, double* matrix) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			scanf_s("%g", &matrix[i * columns + j]);
+		}
+	}
+}
+
+void deleteMatrix_1(int rows, double** matrix) {
+	for (int i = 0; i < rows; i++) {
+		delete matrix[i];
+	}
+}
+
+void showResult_1(int rows, int columns, double** matrix) {
+
+	int sum = sumFunc_1(rows, columns, matrix);
+
+	cout << endl << "Sum numbers of columns with zero:" << sum << endl;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	int rows;
+	cout << "Enter count of rows:";
+	cin >> rows;
+
+	int columns;
+	cout << "Enter count of columns:";
+	cin >> columns;
+
+
+	cout << "First method with two indexes" << endl;
+
+	//Creating array using two indexes
+	double** matrix_1 = new double* [rows];
+	for (int i = 0; i < rows; i++) {
+		matrix_1[i] = new double[columns];
+	}
+	fillArray_1(rows, columns, matrix_1);
+	showResult_1(rows, columns, matrix_1);
+	deleteMatrix_1(rows, matrix_1);
+
+
+	cout << "Second method with one index" << endl;
+
+	//Creating array using one index
+	double* matrix_2 = new double[rows * columns];
+	fillArray_2(rows, columns, matrix_2);
+	
+	system("pause");
+	return 0;
 }
